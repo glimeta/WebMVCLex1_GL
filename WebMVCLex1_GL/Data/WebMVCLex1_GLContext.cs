f46664c77cced3fe0ff8a7cfaaf9b4fda9d99f73
glimeta <117176990+glimeta@users.noreply.github.com>
2022-12-20 12:35:46 +0100

    Add project files.

---
+++ WebMVCLex1_GL/Data/WebMVCLex1_GLContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMVCLex1_GL.Models;
+
+namespace WebMVCLex1_GL.Data
+{
+    public class WebMVCLex1_GLContext : DbContext
+    {
+        public WebMVCLex1_GLContext (DbContextOptions<WebMVCLex1_GLContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebMVCLex1_GL.Models.Car> Car { get; set; } = default!;
+    }
+}
---
+++ WebMVCLex1_GL/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMVCLex1_GL.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+    }
+}
